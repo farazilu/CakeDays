802f9aa7f922203a7e505d325f31fc6f92739d02
faraz <faraz@adeogroup.co.uk>
2018-02-14 10:20:57 +0000

    add composer
    Signed-off-by: faraz <faraz@adeogroup.co.uk>

--- src/CakeDay/CSVHandler.php
+++ src/CakeDay/CSVHandler.php
@@ -9,21 +9,24 @@ namespace CakeDay;
 class CSVHandler implements DataHandelerInterface
 {
 
-    private $_file;
+    private $_file_read;
+
+    private $_file_write;
 
     private $_data;
 
     /**
      */
-    public function __construct($file)
+    public function __construct($file_read, $file_write)
     {
-        $this->_file = $file;
+        $this->_file_read = $file_read;
+        $this->_file_weite = $file_write;
     }
 
     /**
      * (non-PHPdoc)
      *
-     * @see \classes\DataHandelerInterface::setData()
+     * @see \CakeDay\DataHandelerInterface::setData()
      *
      */
     public function setData(array $data)
@@ -34,23 +37,28 @@ class CSVHandler implements DataHandelerInterface
     /**
      * (non-PHPdoc)
      *
-     * @see \classes\DataHandelerInterface::reader()
+     * @see \CakeDay\DataHandelerInterface::reader()
      *
      */
     public function reader()
     {
-        $file = new SplFileObject($this->_file);
-        $file->setFlags(SplFileObject::READ_CSV);
-        foreach ($file as $row) {
-            list ($animal, $class, $legs) = $row;
-            printf("A %s is a %s with %d legs\n", $animal, $class, $legs);
+        $file_info = new SplFileInfo($this->_file_read);
+        if ($file_info->isReadable()) {
+            $file = new SplFileObject($this->_file_read);
+            $file->setFlags(SplFileObject::READ_CSV);
+            foreach ($file as $row) {
+                list ($animal, $class, $legs) = $row;
+                printf("A %s is a %s with %d legs\n", $animal, $class, $legs);
+            }
+        } else {
+            throw new Exception("Sorry input file is not readable, make sure file exist and has correct permissions");
         }
     }
 
     /**
      * (non-PHPdoc)
      *
-     * @see \classes\DataHandelerInterface::writer()
+     * @see \CakeDay\DataHandelerInterface::writer()
      *
      */
     public function writer()
@@ -59,7 +67,7 @@ class CSVHandler implements DataHandelerInterface
     /**
      * (non-PHPdoc)
      *
-     * @see \classes\DataHandelerInterface::getData()
+     * @see \CakeDay\DataHandelerInterface::getData()
      *
      */
     public function getData(): array
